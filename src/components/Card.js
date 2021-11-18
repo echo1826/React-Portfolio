c868d08e1a8b6b3b002b4386fa7ba956881c1c78
Ethan C <ethan.cho1826@gmail.com>
2021-11-18 16:26:08 -0600

    parent, row, card

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Card(props) {
+    return(
+        <div className="">
+            <div>
+
+            </div>
+            <div>{props.children}</div>
+        </div>
+    )
+}
+
+export default Card;
