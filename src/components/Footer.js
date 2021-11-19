c6eca62fffc42127ced883a3c14de0edb5003896
Ethan C <ethan.cho1826@gmail.com>
2021-11-18 18:01:39 -0600

    one footer link done

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer(props) {
+    return(
+        <footer>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="https://github.com/echo1826">Github</a>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer;
