e21b76944422e91aea7478816feefcb263a197eb
Ethan C <ethan.cho1826@gmail.com>
2021-11-18 18:03:17 -0600

    project component

---
+++ src/components/pages/Project.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Project(props) {
+    return(
+        <div className="">
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Project;
