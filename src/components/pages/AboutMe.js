a1e263d46c9d1a1a58f9c085f5cf9e2c8ad73c73
Ethan C <ethan.cho1826@gmail.com>
2021-11-18 22:14:59 -0600

    added info

---
+++ src/components/pages/AboutMe.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AboutMe() {
+    return(
+        <div>
+            <article>
+                <p>
+                    Hi, my name is Ethan Cho and I am currently a web developer in training at the UT Coding Bootcamp. My many hobbies that I enjoy during my free time includes tennis, reading, listening to K-pop, keyboard building, and computer building. I'm proficient in JavaScript with some basic knowledge of C++. The technologies I'm proficient at are react.js, node.js, express.js, bootstrap, mySQL, mongoDB, sequelize, and mongoose.
+                </p>
+                <p>
+                    Software development intrigued me when I began to build computers and started diving into the world of tech when I graduated high school. Eventually as I started to become more comfortable with computer hardware as I researched more for building computers I became interested in learning about software on a computer. So I self-taught myself the basics of programming in C++. I hit a snag along my learning and realized I needed professional help in trying to sort out the information I learned through the bootcamp.
+                </p>
+            </article>
+        </div>
+    )
+}
+
+export default AboutMe;
