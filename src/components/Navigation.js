7d82140fc0c247c9c0a7ba6ad0c9921321bf2f0c
Ethan C <ethan.cho1826@gmail.com>
2021-11-19 00:35:49 -0600

    changed links to a tag, added css classes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,24 @@
 import React from "react";
+import "./styles/Navigation.css";
 
 function Navigation(props) {
     
 
     return(
-        <div>
+        <div className="header">
             <h1>
                 Ethan Cho
             </h1>
             <nav>
-                <ul>
-                    <li>
-                        <a href="#home" onClick={() => props.handlePageChange("About Me")}>About Me</a>
+                <ul className="navbar">
+                    <li className="btn black">
+                        <a className="linkDecoration" href="#home" onClick={() => props.handlePageChange("About Me")}>About Me</a>
                     </li>
-                    <li>
-                        <a href="#projects" onClick={() => props.handlePageChange("Projects")}>Projects</a>
+                    <li className="btn black">
+                        <a className="linkDecoration" href="#projects" onClick={() => props.handlePageChange("Projects")}>Projects</a>
                     </li>
-                    <li>
-                        <a href="#contactme" onClick={() => props.handlePageChange("Contact Me")}>Contact</a>
+                    <li className="btn black">
+                        <a className="linkDecoration" href="#contactme" onClick={() => props.handlePageChange("Contact Me")}>Contact</a>
                     </li>
                 </ul>
             </nav>
