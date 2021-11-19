f80d3a66ece731c3b11f706b2cfeeaf96fda9bf0
Ethan C <ethan.cho1826@gmail.com>
2021-11-18 22:12:33 -0600

    github and linkedin links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function Footer(props) {
+function Footer() {
     return(
         <footer>
             <nav>
                 <ul>
                     <li>
                         <a href="https://github.com/echo1826">Github</a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/ethan-cho-51a138221/">LinkedIn</a>
                     </li>
                 </ul>
             </nav>
