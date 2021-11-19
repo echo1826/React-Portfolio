62d48ddc500c8c7f3be9724b79a166342870ff74
Ethan C <ethan.cho1826@gmail.com>
2021-11-19 02:33:26 -0600

    styling added

--- src/components/pages/ContactForm.js
+++ src/components/pages/ContactForm.js
@@ -4,12 +4,12 @@ import "../styles/ContactForm.css"
 function ContactForm() {
     return(
         <div className="container">
-            <h2>Contact Me</h2>
-            <label htmlFor="name">Name:</label>
+            <h2 style={{marginBottom: 20, fontSize: 40}}>Contact Me</h2>
+            <label style={{fontSize: 20}} htmlFor="name">Name:</label>
             <input className="inputStyling" id="name" type="text" placeholder="Name"></input>
-            <label htmlFor="email">Email:</label>
+            <label style={{fontSize: 20}} htmlFor="email">Email:</label>
             <input className="inputStyling" id="email" type="text" placeholder="Email"></input>
-            <label htmlFor="message">Message:</label>
+            <label style={{fontSize: 20}} htmlFor="message">Message:</label>
             <textarea className="inputStyling" id="message" placeholder="Message" style={{height:300}}></textarea>
             <button className="button" type="submit">Submit</button>
         </div>
