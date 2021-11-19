3f7e767a9d9e1fc2792e7184f02684c4ea6891d4
Ethan C <ethan.cho1826@gmail.com>
2021-11-19 02:34:09 -0600

    more styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,21 +6,21 @@ function Navigation(props) {
 
     return(
         <div className="header">
-            <h1>
+            <h1 className="">
                 Ethan Cho
             </h1>
             <nav>
                 <ul className="navbar">
-                    <li className="btn black">
+                    <li className="btn black spaceOut">
                         <a className="linkDecoration" href="#home" onClick={() => props.handlePageChange("About Me")}>About Me</a>
                     </li>
-                    <li className="btn black">
+                    <li className="btn black spaceOut">
                         <a className="linkDecoration" href="#projects" onClick={() => props.handlePageChange("Projects")}>Projects</a>
                     </li>
-                    <li className="btn black">
+                    <li className="btn black spaceOut">
                         <a className="linkDecoration" href="#contactme" onClick={() => props.handlePageChange("Contact Me")}>Contact</a>
                     </li>
-                    <li className="btn black">
+                    <li className="btn black spaceOut">
                         <a className="linkDecoration" href="#contactme" onClick={() => props.handlePageChange("Resume")}>Resume</a>
                     </li>
                 </ul>
