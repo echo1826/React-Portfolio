f1dd9a8806acc3af1dcad772d5bc8fd2b1a511f1
Ethan C <ethan.cho1826@gmail.com>
2021-11-18 18:01:18 -0600

    basic navbar

---
+++ src/components/Navigation.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Navigation() {
+    
+
+    return(
+        <div>
+            <h1>
+                Ethan Cho
+            </h1>
+            <nav>
+                <ul>
+                    <li>
+                        About Me
+                    </li>
+                    <li>
+                        Projects
+                    </li>
+                    <li>
+                        Contact Me
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Navigation;
