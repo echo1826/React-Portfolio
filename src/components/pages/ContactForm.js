66a6aa2658c2c94c0f837b4428c9417f46a24b09
Ethan C <ethan.cho1826@gmail.com>
2021-11-18 22:33:30 -0600

    button styling

--- src/components/pages/ContactForm.js
+++ src/components/pages/ContactForm.js
@@ -6,12 +6,12 @@ function ContactForm(props) {
         <div className="container">
             <h2>Contact Me</h2>
             <label htmlFor="name">Name:</label>
-            <input id="name" type="text" placeholder="Name"></input>
+            <input className="inputStyling" id="name" type="text" placeholder="Name"></input>
             <label htmlFor="email">Email:</label>
-            <input id="email" type="text" placeholder="Email"></input>
+            <input className="inputStyling" id="email" type="text" placeholder="Email"></input>
             <label htmlFor="message">Message:</label>
-            <textarea id="message" placeholder="Message" style={{height:200}}></textarea>
-            <button>Submit</button>
+            <textarea className="inputStyling" id="message" placeholder="Message" style={{height:300}}></textarea>
+            <button className="button" type="submit">Submit</button>
         </div>
     )
 }
