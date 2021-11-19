976413771e1cad11b6a179d58216aa47aeb7fe9b
Ethan C <ethan.cho1826@gmail.com>
2021-11-19 02:33:08 -0600

    css classes added

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,12 +1,16 @@
 import React from "react";
+import "../styles/AboutMe.css";
 
 function AboutMe() {
     return(
-        <div>
-            <article>
+        <div className="backgroundColor">
+            <h2 className="title">About Me</h2>
+            {/* profile pic goes here */}
+            <article className="article">
                 <p>
                     Hi, my name is Ethan Cho and I am currently a web developer in training at the UT Coding Bootcamp. My many hobbies that I enjoy during my free time includes tennis, reading, listening to K-pop, keyboard building, and computer building. I'm proficient in JavaScript with some basic knowledge of C++. The technologies I'm proficient at are react.js, node.js, express.js, bootstrap, mySQL, mongoDB, sequelize, and mongoose.
                 </p>
+                <br/>
                 <p>
                     Software development intrigued me when I began to build computers and started diving into the world of tech when I graduated high school. Eventually as I started to become more comfortable with computer hardware as I researched more for building computers I became interested in learning about software on a computer. So I self-taught myself the basics of programming in C++. I hit a snag along my learning and realized I needed professional help in trying to sort out the information I learned through the bootcamp.
                 </p>
