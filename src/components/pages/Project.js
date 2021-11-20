cdacee010716abd9095af0c599ebe561a5283997
Ethan C <ethan.cho1826@gmail.com>
2021-11-20 00:42:28 -0600

    project cards done

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -5,10 +5,13 @@ function Project({project}) {
     return(
         
         <li className="card">
-            <a href={project.repo} style={{textDecoration: "none", color: "black"}}>
+            <a href={project.repo} style={{textDecoration: "none", color: "black"}} target="_blank">
                 <img className="image" src={project.image} alt={project.title} />
-                <h3 style={{marginTop: "1rem", fontSize: "1.25rem"}}>{project.title}</h3>
-                <p>{project.stack}</p>
+                <div className="card-body">
+                    <h3 style={{marginTop: "1rem", fontSize: "1.25rem"}}>{project.title}</h3>
+                    <p>{project.stack}</p>
+                    <button className="github-button">Github Repo</button>
+                </div>
             </a>
          </li>
     )
