0e11579b817a3c4045f35fb7cf0685897476b14d
Ethan C <ethan.cho1826@gmail.com>
2021-11-19 20:39:36 -0600

    changed to jsx class

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ function Footer() {
             <nav>
                 <ul className="footerNavbar">
                     <li>
-                        <a className="footerLink" href="https://github.com/echo1826" target="_blank"><i class="fab fa-github fa-5x"></i></a>
+                        <a className="footerLink" href="https://github.com/echo1826" target="_blank"><i className="fab fa-github fa-5x"></i></a>
                     </li>
                     <li>
-                        <a className="footerLink" href="https://www.linkedin.com/in/ethan-cho-51a138221/" target="_blank"><i class="fab fa-linkedin fa-5x"></i></a>
+                        <a className="footerLink" href="https://www.linkedin.com/in/ethan-cho-51a138221/" target="_blank"><i className="fab fa-linkedin fa-5x"></i></a>
                     </li>
                 </ul>
             </nav>
