65c647e9ebb39735ff4b55f6aee973369a6a0144
Ethan C <ethan.cho1826@gmail.com>
2021-11-20 16:49:46 -0600

    getting rid of react warnings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ function Footer() {
             <nav>
                 <ul className="footerNavbar">
                     <li>
-                        <a className="footerLink" href="https://github.com/echo1826" target="_blank"><i className="fab fa-github fa-5x footerIcon"></i></a>
+                        <a className="footerLink" href="https://github.com/echo1826" target="_blank" rel="noreferrer"><i className="fab fa-github fa-5x footerIcon"></i></a>
                     </li>
                     <li>
-                        <a className="footerLink" href="https://www.linkedin.com/in/ethan-cho-51a138221/" target="_blank"><i className="fab fa-linkedin fa-5x footerIcon"></i></a>
+                        <a className="footerLink" href="https://www.linkedin.com/in/ethan-cho-51a138221/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-5x footerIcon"></i></a>
                     </li>
                     <li>
-                        <a className="footerLink" href="https://stackoverflow.com/users/17466878/ephexs" target="_blank"><i className="fab fa-stack-overflow fa-5x footerIcon"></i></a>
+                        <a className="footerLink" href="https://stackoverflow.com/users/17466878/ephexs" target="_blank" rel="noreferrer"><i className="fab fa-stack-overflow fa-5x footerIcon"></i></a>
                     </li>
                 </ul>
             </nav>
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -4,13 +4,13 @@ import "../styles/Project.css";
 function Project({project}) {
     return(
         <li className="card">
-            <a href={project.deployed} style={{textDecoration: "none", color: "black"}} target="_blank">
+            <a href={project.deployed} style={{textDecoration: "none", color: "black"}} target="_blank" rel="noreferrer">
                 <img className="image" src={project.image} alt={project.title} />
             </a>
             <div className="card-body">
                 <h3 style={{marginTop: "1rem", fontSize: "1.25rem"}}>{project.title}</h3>
                 <p>{project.stack}</p>
-                <a href={project.repo} target="_blank">
+                <a href={project.repo} target="_blank" rel="noreferrer">
                     <p className="github-button">Github Repo</p>
                 </a>  
             </div>
