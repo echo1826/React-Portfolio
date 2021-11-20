b2001aac880bc894e8664871251728e03ca8bcc1
Ethan C <ethan.cho1826@gmail.com>
2021-11-19 22:42:00 -0600

    card styling and more

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,12 +1,16 @@
 import React from "react";
+import "../styles/Project.css";
 
-function Project(props) {
+function Project({project}) {
     return(
-        <div className="">
-            <div>
-
-            </div>
-        </div>
+        
+        <li className="card">
+            <a href={project.repo} style={{textDecoration: "none", color: "black"}}>
+                <img className="image" src={project.image} alt={project.title} />
+                <h3 style={{marginTop: "1rem", fontSize: "1.25rem"}}>{project.title}</h3>
+                <p>{project.stack}</p>
+            </a>
+         </li>
     )
 }
 
