4c7ebbc5da56d761649b9588d869f20d7a46e2c1
Ethan C <ethan.cho1826@gmail.com>
2021-11-20 01:21:56 -0600

    added deployment links

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,16 +3,18 @@ import "../styles/Project.css";
 
 function Project({project}) {
     return(
-        
         <li className="card">
-            <a href={project.repo} style={{textDecoration: "none", color: "black"}} target="_blank">
+            <a href={project.deployed} style={{textDecoration: "none", color: "black"}} target="_blank">
                 <img className="image" src={project.image} alt={project.title} />
-                <div className="card-body">
-                    <h3 style={{marginTop: "1rem", fontSize: "1.25rem"}}>{project.title}</h3>
-                    <p>{project.stack}</p>
-                    <button className="github-button">Github Repo</button>
-                </div>
             </a>
+            <div className="card-body">
+                <h3 style={{marginTop: "1rem", fontSize: "1.25rem"}}>{project.title}</h3>
+                <p>{project.stack}</p>
+                <a href={project.repo} target="_blank">
+                    <p className="github-button">Github Repo</p>
+                </a>  
+            </div>
+            
          </li>
     )
 }
