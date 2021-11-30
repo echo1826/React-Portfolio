2d1a64fc759b236e81b471b6d2f64ac05c7e3f74
Ethan C <ethan.cho1826@gmail.com>
2021-11-29 19:25:11 -0600

    refactored repeating code

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,58 +20,56 @@ function Header() {
             }
             case "Projects": {
                 // pass props that give the project data, title, image, technologies used?
-                const projectOne = {
+                const projects = [{
                     title: "Tech Blog",
                     image: "./images/tech-blog.png",
                     stack: "Handlebars, express.js, node.js, sequelize",
                     repo: "https://github.com/echo1826/Tech-Blog",
                     deployed: "https://tech-blog-ec.herokuapp.com/"
-                };
-                const projectTwo = {
+                },
+                {
                     title: "Express.gym",
                     image: "../images/express-gym.png",
                     stack: "Handlebars, express.js, node.js, sequelize",
                     repo: "https://github.com/nicoledodge/express.gym",
                     deployed: "https://express-gym.herokuapp.com/"
-                };
-                const projectThree = {
+                },
+                {
                     title: "Mafia Employee Tracker backend",
                     image: "./images/mafia-tracker.PNG",
                     stack: "mySQL, node.js, express.js",
                     repo: 'https://github.com/echo1826/Mafia-Employee-Tracker',
                     deployed: "https://watch.screencastify.com/v/f5uqZP3bk0R7u0S3FCI8"
-                };
-                const projectFour = {
+                },
+                {
                     title: "National Parks Wildfire Tracker",
                     image: "./images/wildfire-tracker.PNG",
                     stack: "HTML, CSS, JavaScript",
                     repo: "https://github.com/chuck2076/Weather_Wildfire_App",
                     deployed: "https://chuck2076.github.io/Weather_Wildfire_App/"
-                };
-                const projectFive = {
+                },
+                {
                     title: "Fitness Tracker",
                     image: "./images/fitness-tracker.PNG",
                     stack: "HTML, mongoDB, node.js, express.js",
                     repo: "https://github.com/echo1826/Fitness-Tracker",
                     deployed: "https://ec-fitness-tracker.herokuapp.com/?id=6192c8bdb3399b7d14624dbc"
-                };
-                const projectSix = {
+                },
+                {
                     title: "Weather Tracker",
                     image: "./images/weather-app.png",
                     stack: "HTML, CSS, JavaScript, jQuery",
                     repo: "https://github.com/echo1826/Weather-Checker",
                     deployed: "https://echo1826.github.io/Weather-Checker/"
-                };
+                }
+                ]
                 return(
                     <div>
                         <h3 style={{marginLeft: "2%"}}>Click the images to see the application in action!</h3>
                         <ul className="card-wrapper">
-                            <Project project={projectOne}/>
-                            <Project project={projectTwo}/>
-                            <Project project={projectThree}/>
-                            <Project project={projectFour}/>
-                            <Project project={projectFive}/>
-                            <Project project={projectSix}/>
+                            {projects.map((project) => {
+                                return <Project project={project} />;
+                            })}
                         </ul>
                     </div>
                     
