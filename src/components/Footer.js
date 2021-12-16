229d219bcf3bf6785c3035c32086ca57c860eb54
Ethan C <ethan.cho1826@gmail.com>
2021-12-16 17:54:05 -0600

    added email link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import "./styles/Footer.css";
 function Footer() {
     return(
         <footer>
+            <h2 style={{textAlign: "center", fontSize: "20px"}}>Check out my socials, or email me here!</h2>
             <nav >
                 <ul style={{margin: 40, position: "relative", bottom: 0}} className="footerNavbar">
                     <li>
@@ -14,6 +15,9 @@ function Footer() {
                     </li>
                     <li>
                         <a className="footerLink" href="https://stackoverflow.com/users/17466878/ephexs" target="_blank" rel="noreferrer"><i className="fab fa-stack-overflow fa-5x footerIcon"></i></a>
+                    </li>
+                    <li>
+                        <a className="footerLink" href="mailto:ethan.cho1826@gmail.com"><i class="fas fa-envelope fa-5x footerIcon"></i></a>
                     </li>
                 </ul>
             </nav>
